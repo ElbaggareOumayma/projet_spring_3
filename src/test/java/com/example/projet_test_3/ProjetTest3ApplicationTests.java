b600abfc5554ee1b708b1304470c91060afa6f6a
oumaimaelbaggare <elbaggareoumayma@gmail.com>
2023-04-29 23:33:06 +0100

    First commit

---
+++ src/test/java/com/example/projet_test_3/ProjetTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_test_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
